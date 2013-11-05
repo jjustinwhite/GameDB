2469c61a3f6be61c4bdaa199dc3c6db9cb0dbc40
SirJustinWhite <Jjustinwhite@mac.com>
2013-11-04 21:36:32 -0600

    user signup form completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GameDB::Application.routes.draw do
-  get "users/new"
+  resources :users
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
