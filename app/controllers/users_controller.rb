d36c19322d33a4256d1c2ae8c0a345b561bf4fb4
SirJustinWhite <Jjustinwhite@mac.com>
2013-11-05 04:45:04 -0600

    Finish users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -26,6 +27,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
 
   def update
